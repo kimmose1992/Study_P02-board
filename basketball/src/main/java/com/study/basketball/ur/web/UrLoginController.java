814740ad4178d4adb85528dafa3192d01da958ea
kimmose1992 <kimmose1992@github.com>
2022-06-04 22:42:30 +0900

    1. 환경설정 값 적용 (Port, Path, DB) 2. Spring Security 임시 주석 3. 로그인 페이지 초기 생성

---
+++ basketball/src/main/java/com/study/basketball/ur/web/UrLoginController.java
@@ -0,0 +1,27 @@
+package com.study.basketball.ur.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @title	: [공통] 메인 Controller 클래스	  
+ * @author	: 김모세
+ * @create	: 2022.06.04
+ */
+@Controller
+@RequestMapping("/ur/")
+public class UrLoginController {
+
+	/**
+	 * @title   : 로그인 화면
+	 * @method  : login()
+	 * @comment : 로그인 화면으로 이동
+	 * @param   : N/A
+	 */
+	@RequestMapping("login")
+	public ModelAndView login(ModelAndView mav) throws Exception {
+		mav.setViewName("basketball/ur/VWUR0001");
+		return mav;
+	}
+}
