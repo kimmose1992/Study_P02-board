6fa96106bf7e2576e171b56200b47815ae4cd48f
kimmose1992 <kimmose1992@gmail.com>
2022-06-06 01:04:16 +0900

    1. Gradle - Spring Security 추가 2. Security 기본 설정 (로그인 페이지 설정 및 허용)

---
+++ basketball/src/main/java/com/study/basketball/cm/security/CmSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.study.basketball.cm.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @title	: Spring Security 설정 클래스	  
+ * @author	: 김모세
+ * @create	: 2022.06.06
+ */
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class CmSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			/** Intercept URL */
+			.authorizeRequests()
+			.antMatchers("/", "/ur/login", "/ur/signUp").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			
+			/** Login */
+			.formLogin()
+			.loginPage("/ur/VWUR0001")
+			.usernameParameter("userId")
+			.passwordParameter("userPw")
+			.loginProcessingUrl("/signIn")
+			.permitAll()
+			.and()
+			
+			/** Logout */
+			.logout()
+			.invalidateHttpSession(true)
+			.and()
+			//.exceptionHandling()
+			//.accessDeniedHandler()
+		
+			/** Header */
+			.headers()
+			.cacheControl().disable()
+			.frameOptions().sameOrigin();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**")
+					  .antMatchers("/js/**")
+					  .antMatchers("/images/**");
+	}
+}
